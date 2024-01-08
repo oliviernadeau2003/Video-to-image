7f9016516b2238f929f9e8f767e414428ac1b7ca
oliviernadeau2003 <2148279@cstj.qc.ca>
2024-01-07 19:10:00 -0500

    updated

--- video_to_image.py
+++ video_to_image.py
@@ -1,48 +1,3 @@
-# import cv2
-# import os
-
-
-# def convert_video_to_images(
-#     video_path, output_folder, image_prefix="frame", precision=1
-# ):
-#     cap = cv2.VideoCapture(video_path)
-#     os.makedirs(output_folder, exist_ok=True)
-
-#     fps = cap.get(cv2.CAP_PROP_FPS)
-#     frames_to_skip = int(fps) if fps > 0 else 1
-#     frames_to_skip *= precision  # Multiply frames_to_skip by precision
-
-#     count = 0
-
-#     while True:
-#         ret, frame = cap.read()
-
-#         if not ret:
-#             break
-
-#         if count % frames_to_skip == 0:
-#             image_filename = f"{image_prefix}_{count // frames_to_skip:04d}.png"
-#             image_path = os.path.join(output_folder, image_filename)
-#             cv2.imwrite(image_path, frame)
-
-#         count += 1
-
-#     cap.release()
-
-
-# if __name__ == "__main__":
-#     input_video_path = "clip.mp4"
-#     output_images_folder = "output_images"
-
-#     # Set the precision (number of frames to skip) - change this value as needed
-#     precision_value = float(
-#         input("Choose The Precision(Higher Value Give Less Precision) : ") or 1
-#     )
-
-#     convert_video_to_images(
-#         input_video_path, output_images_folder, precision=precision_value
-#     )
-
 import cv2
 import os
 
@@ -54,8 +9,9 @@ def convert_video_to_images(
     os.makedirs(output_folder, exist_ok=True)
 
     fps = cap.get(cv2.CAP_PROP_FPS)
+    fps = fps / precision
     frames_to_skip = int(fps) if fps > 0 else 1
-    frames_to_skip *= precision
+    # frames_to_skip *= precision
 
     count = 1  # Start the count from 1
     image_number = 0
@@ -80,12 +36,17 @@ def convert_video_to_images(
 
 
 if __name__ == "__main__":
-    input_video_path = "clip.mp4"
-    output_images_folder = "output_images"
+    input_video_path = input("Video Path (Default: clip.mp4) : ") or "./clip.mp4"
+    output_images_folder = (
+        input("Output Folder (Default: video_to_images_output) : ")
+        or "video_to_images_output"
+    )
 
     # Set the precision (number of frames to skip) - change this value as needed
     precision_value = float(
-        input("Choose The Precision(Higher Value Give Less Precision [~Image/Sec~]) : ")
+        input(
+            "Choose The Precision(Higher Value Give Higher Precision [~Image/Sec~]) (Default: 1) : "
+        )
         or 1
     )
 
